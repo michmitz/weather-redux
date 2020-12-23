29f4e6ad199a32db74168202ec9062b54f429a93
Michelle Stermitz <stermitzm@gmail.com>
2020-12-23 00:51:21 -0800

    Added assets with getGif function

--- src/components/app/DayCarouselItem/DayCarouselItem.jsx
+++ src/components/app/DayCarouselItem/DayCarouselItem.jsx
@@ -2,24 +2,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import dayCarouselItemStyles from './DayCarouselItem.css';
+import { getGif } from '../../../services/getGif';
 
-const DayCarouselItem = ({ date, currentTemp, hi, lo, description }) => (
-  <div className={dayCarouselItemStyles.dayDiv} data-testid="day-div">
-    <h2 className={dayCarouselItemStyles.date}>{date}</h2>
-    <h3 className={dayCarouselItemStyles.currentTemp}>Current Temp: {currentTemp}˚</h3>
-    <p>Hi: {hi}˚</p>
-    <p>Lo: {lo}˚</p>
-    <p>{description}</p>
-    <img src={'/assets/partly-cloudy-day.gif'} alt="partly cloudy" />
-  </div> 
-);
+const DayCarouselItem = ({ date, currentTemp, hi, lo, description, imgCode }) => {
+  const imgSrc = getGif(imgCode);
+  
+  return (
+    <div className={dayCarouselItemStyles.dayDiv} data-testid="day-div">
+      <h2 className={dayCarouselItemStyles.date}>{date}</h2>
+      <h3 className={dayCarouselItemStyles.currentTemp}>Current Temp: {currentTemp}˚</h3>
+      <p>Hi: {hi}˚</p>
+      <p>Lo: {lo}˚</p>
+      <p>{description}</p>
+      <img src={`/assets/${imgSrc}`} alt="partly cloudy" />
+    </div>
+  );
+};
 
 DayCarouselItem.propTypes = {
   date: PropTypes.string.isRequired,
   currentTemp: PropTypes.number.isRequired,
   hi: PropTypes.number.isRequired,
   lo: PropTypes.number.isRequired,
-  description: PropTypes.string.isRequired
+  description: PropTypes.string.isRequired,
+  imgCode: PropTypes.number.isRequired
 };
 
 export default DayCarouselItem;
